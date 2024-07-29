c84d3a6661d08c53a88966b2b80cd1095afcb3f0
Flobster23 <101504463+Flobster23@users.noreply.github.com>
2024-07-29 11:41:36 -0300

    Add files via upload

--- Tesouro.cpp
+++ Tesouro.cpp
@@ -1,53 +1,49 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <ctime>
 
 #define rTotais 36
 
 using namespace std;
 
 int main(){
-    int i, j, nJogadores, rJogador, D = sqrt(rTotais), mina[D][D];
-    string J1, J2, J3, J4;
+    int i, j, nJogadores, rJogador, D = sqrt(rTotais), l, c, pistas, armadi, dima; 
+    char P1 = 65, P2 = 49;
+    string minaE[D][D], mina[D][D];
     cout << "Bem vindos a mina de diamantes do antigo imperador Skar! Vocês foram os escolhidos para encontrar os tesouros perdidos, prestem muita atenção nas pistas e cuidado com as armadilhas, boa sorte." << endl;
-    cout << endl << "Escolha a número de jogadores:" << endl;
+    cout << endl << "Escolha o número de jogadores (de 2 a 4):" << endl;
     cin >> nJogadores;
+    string jogadores[nJogadores + 1];
     rJogador = rTotais / nJogadores;
     cout << endl;
+    srand(time(NULL));
+    pistas = ceil(0.05 * rTotais);
+    armadi = ceil(0.1 * rTotais);
+    dima = ceil(0.6 * rTotais);
 
-    switch (nJogadores){
-    case 2:
-        cin.ignore();
-        cout << "identifique-se, jogador 1:" << endl;
-        getline(cin, J1);
-        cout << "identifique-se, jogador 2:" << endl;
-        getline(cin, J2);
-        J3 = '0';
-        J4 = '0';
-        break;
-
-    case 3:
-        cin.ignore();
-        cout << "identifique-se, jogador 1:" << endl;
-        getline(cin, J1);
-        cout << "identifique-se, jogador 2:" << endl;
-        getline(cin, J2);
-        cout << "identifique-se, jogador 3:" << endl;
-        getline(cin, J3);
-        J4 = '0';
-        break;
-
-    case 4:
-        cin.ignore();
-        cout << "identifique-se, jogador 1:" << endl;
-        getline(cin, J1);
-        cout << "identifique-se, jogador 2:" << endl;
-        getline(cin, J2);
-        cout << "identifique-se, jogador 3:" << endl;
-        getline(cin, J3);
-        cout << "identifique-se, jogador 4:" << endl;
-        getline(cin, J4);
-        break; 
+    for(l = 0; l < D; l++){
+        for(c = 0; c < D; c++){
+            minaE[l][c].append(1,P1);
+            minaE[l][c].append(1,P2);
+            P2++;
+        }
+        P2 = 49;
+        P1++;
     }
+
+    for(i = 1; i <= nJogadores; i++){
+        cout << "Identifique-se, jogador " << i << ":" << endl;
+        cin.ignore();
+        getline(cin, jogadores[i]);
+        cout << endl;
+    }
+
+    for(l = 0; l < D; l++){
+        for(c = 0; c < D; c++){
+            
+        }
+    }
+    
 
 }
